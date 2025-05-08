51519a83ffb7a1c1fe8e14854b01dde041f5042a
Maryana Golik <golikmaryana307@gmail.com>
2025-05-08 17:32:55 +0300

    add file

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+(() => {
+  const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+      burgerBtn: document.querySelector("#toggleButton"),
+      mobMenu: document.querySelector("#mob-menu"),
+  };
+
+  // Модальне вікно
+  if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+      refs.openModalBtn.addEventListener("click", toggleModal);
+      refs.closeModalBtn.addEventListener("click", toggleModal);
+  }
+
+  function toggleModal() {
+      refs.modal.classList.toggle("is-open");
+  }
+
+  // Мобільне меню (бургер кнопка)
+  if (refs.burgerBtn && refs.mobMenu) {
+      refs.burgerBtn.addEventListener("click", () => {
+          refs.mobMenu.classList.toggle("is-open-mob");
+      });
+
+      // Закриття моб. меню по хрестику
+      const closeBtn = document.querySelector("#close-btn");
+      if (closeBtn) {
+          closeBtn.addEventListener("click", () => {
+              refs.mobMenu.classList.remove("is-open-mob");
+          });
+      }
+  }
+})();
